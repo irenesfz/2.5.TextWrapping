9aa43a67773dffbb8eb1ad1d175706c8c3fc32b2
irene <irene.sanchez@iesdoctorbalmis.com>
2021-09-29 12:16:40 +0200

    ejercicio terminado

--- TextWrapping/MainWindow.xaml.cs
+++ TextWrapping/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TextWrapping
 {
@@ -23,6 +13,40 @@ namespace TextWrapping
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ResultadoTextBlock.Text = TextoTextBox.Text;
+        }
+
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (NegritaCheckBox.IsChecked == true)
+                ResultadoTextBlock.FontWeight = FontWeights.Bold;
+            else ResultadoTextBlock.FontWeight = FontWeights.Normal;
+        }
+
+        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (CursivaCheckBox.IsChecked == true)
+                ResultadoTextBlock.FontStyle = FontStyles.Italic;
+            else ResultadoTextBlock.FontStyle = FontStyles.Normal;
+        }
+
+        private void AzulRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.Foreground = Brushes.Blue;
+        }
+
+        private void RojoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.Foreground = Brushes.Red;
+        }
+
+        private void VerdeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.Foreground = Brushes.Green;
         }
     }
 }
